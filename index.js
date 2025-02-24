ae417fe8b9f212c3526b424715751836d3d487ce
khadija-urmi <khadija5jahan211@gmail.com>
2025-02-24 21:38:51 +0600

    add user in userCollection

--- index.js
+++ index.js
@@ -24,14 +24,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-   
-    await client.connect();
-    
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const userCollection = client.db("taskManager").collection("users");
+    const taskCollection = client.db("taskManager").collection("taskInfo");
+  
+app.post('/user',async(req,res)=>{
+    const user = req.body
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+})
+
   } finally {
    
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
