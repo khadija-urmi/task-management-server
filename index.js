65d729e18089a20e2475e1315915703201d41a51
khadija-urmi <khadija5jahan211@gmail.com>
2025-02-26 12:28:59 +0600

    add all task

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pbpi8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -26,11 +26,29 @@ async function run() {
   try {
     const userCollection = client.db("taskManager").collection("users");
     const taskCollection = client.db("taskManager").collection("taskInfo");
-  
+//add a user  
 app.post('/user',async(req,res)=>{
     const user = req.body
       const result = await userCollection.insertOne(user);
       res.send(result)
+})
+
+//add a task
+app.post('/tasks',async(req,res)=>{
+  const task = req.body;
+  const result = await taskCollection.insertOne(task);
+  const filter ={_id: new ObjectId(result.insertedId)};
+
+  const updateDoc = { $inc: {order: 1} };
+  const updateResult = await taskCollection.updateOne(filter, updateDoc);
+
+  res.send({ result: result, updateResult: updateResult });
+})
+
+//get all task
+app.get('/tasks',async(req,res)=>{
+  const result = await taskCollection.find().toArray();
+  res.send(result)
 })
 
   } finally {
